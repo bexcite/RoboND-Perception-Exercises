69ce2bb917526267291e24fb365fc63a1fac8df2
bexcite <pavel.bashmakov@gmail.com>
2017-08-21 23:00:23 -0700

    Ransac and segmentation

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -24,18 +24,51 @@ pcl.save(cloud_filtered, filename)
 
 
 # ==== PassThrough filter
+# Create a PassThrough filter object.
+passthrough = cloud_filtered.make_passthrough_filter()
 
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name(filter_axis)
+axis_min = 0.6
+axis_max = 1.1
+passthrough.set_filter_limits(axis_min, axis_max)
 
-# RANSAC plane segmentation
+# Finally use the filter function to obtain the resultant point cloud. 
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
+# ===== RANSAC plane segmentation
+
+# Create the segmentation object
+seg = cloud_filtered.make_segmenter()
+
+# Set the model you wish to fit 
+seg.set_model_type(pcl.SACMODEL_PLANE)
+seg.set_method_type(pcl.SAC_RANSAC)
+
+# Max distance for a point to be considered fitting the model
+# Experiment with different values for max_distance 
+# for segmenting the table
+max_distance = 0.01
+seg.set_distance_threshold(max_distance)
+
+# Call the segment function to obtain set of inlier indices and model coefficients
+inliers, coefficients = seg.segment()
 
 # Extract inliers
+extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+filename = 'extracted_inliers.pcd'
+pcl.save(extracted_inliers, filename)
 
 # Save pcd for table
 # pcl.save(cloud, filename)
 
 
 # Extract outliers
-
-
+extracted_inliers = cloud_filtered.extract(inliers, negative=True)
+filename = 'extracted_outliers.pcd'
 # Save pcd for tabletop objects
+pcl.save(extracted_inliers, filename) 
+
